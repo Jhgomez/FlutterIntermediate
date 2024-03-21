84a8bd1e83e42c6c6d29beb682f1bb602f2965cb
Juan Gomez <juan.e.g.f@outlook.com>
2024-03-21 11:42:42 -0600

    chore: Add logic to create and remove chips

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,54 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  int counter = 0;
+  List<Widget> items = [];
+
+  @override
+  void initState () {
+    super.initState();
+    for(int i = 0; i < 5; i++) {
+      Widget child = _newItem(i)
+      items.add(child);
+    }
+  }
+
+  void _onClick() {
+    Widget child = _newItem(counter);
+    setState(() => items.add(child));
+  }
+
+  Widget _newItem(int i) {
+    Key key = Key('item_$i');
+    Container child = Container(
+      key: key,
+      child: Chip(
+        label: Text('$i Name here'),
+        deleteIconColor: Colors.red,
+        deleteButtonTooltipMessage: 'Delete',
+        onDeleted: () => _removeItem(key),
+        avatar: CircleAvatar(
+          backgroundColor: Colors.grey.shade500,
+          child: Text(i.toString()),
+          
+          ),
+        ),
+    );
+
+    return child;
+  }
+
+  void _removeItem(Key key) {
+    for(int i = 0; i < items.length; i++) {
+      Widget child = items.elementAt(i);
+      if(child.key == key) {
+        setState(() => items.removeAt(i));
+        print('Removing ${key.toString()}');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     throw Scaffold(
