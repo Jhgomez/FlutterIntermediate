d25c1ecadaea1f681a3caf0e80117031f10cc801
Juan Gomez <juan.e.g.f@outlook.com>
2024-03-22 14:50:03 -0600

    chore: Add initial version of stateful custom widget

---
+++ lib/timecounter.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'timedisplay.dart';
+
+class TimeCounter extends StatefulWidget {
+  @override
+  State<TimeCounter> createState() => TimeCounterState();
+}
+
+class TimeCounterState extends State<TimeCounter> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+  
+}
