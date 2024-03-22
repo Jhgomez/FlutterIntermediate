5e9135b16296b88b0fd6b19db743559c1f8d1c7f
Juan Gomez <juan.e.g.f@outlook.com>
2024-03-22 14:49:29 -0600

    chore: add stateles customs widget

---
+++ lib/timedisplay.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TimeDisplay extends StatelessWidget {
+
+  TimeDisplay({super.key, this.color = Colors.green, this.duration = const Duration(), this.onClear});
+  
+  Duration duration;
+  Color color;
+  final ValueChanged<Duration>? onClear;
+
+  void onClick() {
+    onClear??(duration);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(5),
+          child: Text(
+            duration.toString(),
+            style: TextStyle(
+              fontSize: 32,
+              color: color
+              ),
+            ),
+          ),
+        IconButton(
+          onPressed: onClick, 
+          icon: const Icon(Icons.clear)
+          )
+      ],
+    );
+  }
+
+}
