cc79498bbb526a007c7542f08d3b295c8972dbcf
Juan Gomez <juan.e.g.f@outlook.com>
2024-03-24 20:30:46 -0600

    chore: Add widgets to custom stateful widget

--- lib/timecounter.dart
+++ lib/timecounter.dart
@@ -9,10 +9,67 @@ class TimeCounter extends StatefulWidget {
 
 class TimeCounterState extends State<TimeCounter> {
   
+  late Stopwatch watch;
+  late Timer timer;
+  late  Duration duration;
+
+  void onStart() {
+
+  }
+
+  void onStop() {
+
+  }
+
+  void onTimeOut(Timer timer) {
+
+  }
+
+  void onClear(Duration value) {
+
+  }
+
+  @override
+  void initState() {
+    duration = Duration();
+  
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Container(
+      padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
+      child: Center(
+        child: Column(
+          children: [
+            TimeDisplay(
+              color: Colors.red,
+              duration: duration,
+              onClear: onClear,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    onPressed: onStart,
+                    child: const Text('Start'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    onPressed: onStop,
+                    child: const Text('Stop'),
+                  ),
+                )
+              ],
+            )
+          ]
+          )
+        ),
+    );
   }
   
 }
