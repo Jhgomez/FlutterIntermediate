0668b6d03fdea9171e70a679c227f16b99e22ce0
Juan Gomez <juan.e.g.f@outlook.com>
2024-03-25 17:11:58 -0600

    chore: call custom widget from second screen

--- lib/screens/secondscreen.dart
+++ lib/screens/secondscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '/mywidget.dart';
 
 class SecondScreen extends StatefulWidget {
   @override
@@ -26,7 +27,8 @@ class SecondState extends State<SecondScreen> {
                 Navigator.of(context).pop();
               }, 
               child: const Text('Back')
-              )
+            ),
+            MyWidget()
           ],
         )
     );
